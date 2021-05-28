e6c8c96c4adb76b0ebb2178d77eae79a2cb5fc9a
Gaston <mail@gastonotero.com>
2021-05-28 19:44:14 -0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,63 +1,59 @@
 import "./App.css";
 import { Form, Button, Container, Row, Col, Table } from "react-bootstrap";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 function App() {
   const [simulacionData, setSimulacionData] = useState({
-    ultimoReloj: null,
-    relojDesde: null,
-    relojHasta: null,
-    probabilidadComprar: null,
-    probabilidadReparar: null,
-    probabilidadRetirar: null,
-    repUniformeA: null,
-    repUniformeB: null,
-    ventaUniformeA: null,
-    ventaUniformeB: null,
-    llegadaClienteA: null,
-    llegadaClienteB: null,
+    ultimoReloj: 1000,
+    relojDesde: 115,
+    relojHasta: 300,
+    probabilidadComprar: 0.45,
+    probabilidadReparar: 0.25,
+    probabilidadRetirar: 0.3,
+    repUniformeA: 18,
+    repUniformeB: 22,
+    ventaUniformeA: 6,
+    ventaUniformeB: 10,
+    llegadaClienteA: 13,
+    llegadaClienteB: 17,
   });
 
   const [simulacion, setSimulacion] = useState(null);
 
   const [validForm, setValidForm] = useState(false);
 
-  const {
-    ultimoReloj,
-    relojDesde,
-    relojHasta,
-    probabilidadComprar,
-    probabilidadReparar,
-    probabilidadRetirar,
-    repUniformeA,
-    repUniformeB,
-    ventaUniformeA,
-    ventaUniformeB,
-    llegadaClienteA,
-    llegadaClienteB,
-  } = simulacionData;
+  const swalClientes = withReactContent(Swal);
 
   const updateForm = (e) => {
-    let value = parseInt(e.target.value);
-    if (isNaN(value)) {
-      setSimulacionData({
-        ...simulacionData,
-        [e.target.name]: "",
-      });
-    } else {
-      if (value) {
-        setSimulacionData({
-          ...simulacionData,
-          [e.target.name]: value,
-        });
-      }
-    }
+    console.log(e.target.value);
+    setSimulacionData({
+      ...simulacionData,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const fetchInformation = async () => {
     const BASE_URL = "http://172.105.159.186:5001";
     try {
+      let {
+        ultimoReloj,
+        relojDesde,
+        relojHasta,
+        probabilidadComprar,
+        probabilidadReparar,
+        probabilidadRetirar,
+        repUniformeA,
+        repUniformeB,
+        ventaUniformeA,
+        ventaUniformeB,
+        llegadaClienteA,
+        llegadaClienteB,
+      } = simulacionData;
+
       const response = await axios.post(`${BASE_URL}/api/run-simulation`, {
         ultimoReloj,
         relojDesde,
@@ -72,13 +68,30 @@ function App() {
         llegadaClienteA,
         llegadaClienteB,
       });
-      console.log(response);
+
+      console.log(response.data);
+      setSimulacion(response.data);
     } catch (error) {
       console.log(error);
     }
   };
 
   const checkForm = () => {
+    let {
+      ultimoReloj,
+      relojDesde,
+      relojHasta,
+      probabilidadComprar,
+      probabilidadReparar,
+      probabilidadRetirar,
+      repUniformeA,
+      repUniformeB,
+      ventaUniformeA,
+      ventaUniformeB,
+      llegadaClienteA,
+      llegadaClienteB,
+    } = simulacionData;
+
     if (
       ultimoReloj === null ||
       relojDesde === null ||
@@ -100,6 +113,7 @@ function App() {
   };
 
   const startSimulation = (e) => {
+    return fetchInformation();
     e.preventDefault();
     checkForm();
     if (!validForm) {
@@ -110,7 +124,7 @@ function App() {
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       <Container fluid>
         <Row>
           <Col>
@@ -123,17 +137,17 @@ function App() {
           </p>
         )}
 
-        <div className='input-fields'>
+        <div className="input-fields">
           <Row>
             <Form>
               <Form.Group>
-                <Form.Label>Número de Simulaciones</Form.Label>
+                <Form.Label>Fin del reloj</Form.Label>
                 <Form.Control
-                  type='text'
-                  placeholder='Ingresar número de simulaciones'
-                  className='simulation-input'
-                  value={ultimoReloj}
-                  name='ultimoReloj'
+                  type="text"
+                  placeholder="Ingresar número de simulaciones"
+                  className="simulation-input"
+                  value={simulacionData.ultimoReloj}
+                  name="ultimoReloj"
                   onChange={updateForm}
                 />
               </Form.Group>
@@ -144,11 +158,11 @@ function App() {
               <Form.Group>
                 <Form.Label>Mostrar Reloj desde</Form.Label>
                 <Form.Control
-                  type='text'
-                  placeholder='Ingresar Reloj de inicio'
-                  className='simulation-input'
-                  value={relojDesde}
-                  name='relojDesde'
+                  type="text"
+                  placeholder="Ingresar Reloj de inicio"
+                  className="simulation-input"
+                  value={simulacionData.relojDesde}
+                  name="relojDesde"
                   onChange={updateForm}
                 />
               </Form.Group>
@@ -159,11 +173,11 @@ function App() {
               <Form.Group>
                 <Form.Label>Mostrar Reloj hasta</Form.Label>
                 <Form.Control
-                  type='text'
-                  placeholder='Ingresar Reloj de fin'
-                  className='simulation-input'
-                  value={relojHasta}
-                  name='relojHasta'
+                  type="text"
+                  placeholder="Ingresar Reloj de fin"
+                  className="simulation-input"
+                  value={simulacionData.relojHasta}
+                  name="relojHasta"
                   onChange={updateForm}
                 />
               </Form.Group>
@@ -172,9 +186,9 @@ function App() {
         </div>
 
         <Row>
-          <Col lg='14'>
+          <Col lg="14">
             <h1>Distribuciones</h1>
-            <Table striped bordered hover variant='dark' size='sm'>
+            <Table striped bordered hover variant="dark" size="sm">
               <thead>
                 <tr>
                   <th>Distribuciones</th>
@@ -187,16 +201,16 @@ function App() {
                   <td>
                     Distribución Uniforme entre{" "}
                     <input
-                      type='text'
-                      value={llegadaClienteA}
-                      name='llegadaClienteA'
+                      type="text"
+                      value={simulacionData.llegadaClienteA}
+                      name="llegadaClienteA"
                       onChange={updateForm}
                     ></input>{" "}
                     y{" "}
                     <input
-                      type='text'
-                      value={llegadaClienteB}
-                      name='llegadaClienteB'
+                      type="text"
+                      value={simulacionData.llegadaClienteB}
+                      name="llegadaClienteB"
                       onChange={updateForm}
                     />
                   </td>
@@ -206,16 +220,16 @@ function App() {
                   <td>
                     Distribución Uniforme entre{" "}
                     <input
-                      type='text'
-                      value={ventaUniformeA}
-                      name='ventaUniformeA'
+                      type="text"
+                      value={simulacionData.ventaUniformeA}
+                      name="ventaUniformeA"
                       onChange={updateForm}
                     ></input>{" "}
                     y{" "}
                     <input
-                      type='text'
-                      value={ventaUniformeB}
-                      name='ventaUniformeB'
+                      type="text"
+                      value={simulacionData.ventaUniformeB}
+                      name="ventaUniformeB"
                       onChange={updateForm}
                     />
                   </td>
@@ -225,16 +239,16 @@ function App() {
                   <td>
                     Distribución Uniforme entre{" "}
                     <input
-                      type='text'
-                      value={repUniformeA}
-                      name='repUniformeA'
+                      type="text"
+                      value={simulacionData.repUniformeA}
+                      name="repUniformeA"
                       onChange={updateForm}
                     ></input>{" "}
                     y{" "}
                     <input
-                      type='text'
-                      value={repUniformeB}
-                      name='repUniformeB'
+                      type="text"
+                      value={simulacionData.repUniformeB}
+                      name="repUniformeB"
                       onChange={updateForm}
                     />
                   </td>
@@ -247,9 +261,9 @@ function App() {
             </Table>
           </Col>
 
-          <Col xs lg='4'>
+          <Col xs lg="4">
             <h1>Probabilidades</h1>
-            <Table striped bordered hover variant='dark' size='sm'>
+            <Table striped bordered hover variant="dark" size="sm">
               <thead>
                 <tr>
                   <th>Tipo de Atención</th>
@@ -261,11 +275,11 @@ function App() {
                   <td>Comprar</td>
                   <td>
                     <input
-                      type='text'
-                      value={probabilidadComprar}
-                      name='probabilidadComprar'
+                      type="text"
+                      value={simulacionData.probabilidadComprar}
+                      name="probabilidadComprar"
                       onChange={updateForm}
-                      placeholder='Ingrese la probabilidad de que un cliente compre un reloj'
+                      placeholder="Ingrese la probabilidad de que un cliente compre un reloj"
                     />{" "}
                     %
                   </td>
@@ -274,11 +288,11 @@ function App() {
                   <td>Reparar</td>
                   <td>
                     <input
-                      type='text'
-                      value={probabilidadReparar}
-                      name='probabilidadReparar'
+                      type="text"
+                      value={simulacionData.probabilidadReparar}
+                      name="probabilidadReparar"
                       onChange={updateForm}
-                      placeholder='Ingrese la probabilidad de que un cliente repare un reloj'
+                      placeholder="Ingrese la probabilidad de que un cliente repare un reloj"
                     />{" "}
                     %
                   </td>
@@ -287,11 +301,11 @@ function App() {
                   <td>Retirar relojes</td>
                   <td>
                     <input
-                      type='text'
-                      value={probabilidadRetirar}
-                      name='probabilidadRetirar'
+                      type="text"
+                      value={simulacionData.probabilidadRetirar}
+                      name="probabilidadRetirar"
                       onChange={updateForm}
-                      placeholder='Ingrese la probabilidad de que un cliente retire un reloj'
+                      placeholder="Ingrese la probabilidad de que un cliente retire un reloj"
                     />{" "}
                     %
                   </td>
@@ -303,9 +317,8 @@ function App() {
         <br />
 
         <Button
-          variant='primary'
-          lg
-          className='simulate-btn'
+          variant="primary"
+          className="simulate-btn"
           onClick={startSimulation}
         >
           SIMULAR
@@ -322,16 +335,17 @@ function App() {
           <Col>
             <h1>SIMULACIÓN</h1>
           </Col>
-          <Table striped bordered hover variant='dark'>
+          <Table striped bordered hover variant="dark">
             <thead>
-              <th colSpan={3}>EVENTOS</th>
-              <th colSpan={3}>LLEGADA CLIENTE</th>
-              <th colSpan={2}>ATENCIÓN</th>
-              <th colSpan={2}>DEMORA</th>
-              <th colSpan={5}>AYUDANTE</th>
-              <th colSpan={4}>RELOJERO</th>
-              <th colSpan={1}>TIENDA</th>
-
+              <tr>
+                <th colSpan={3}>EVENTOS</th>
+                <th colSpan={3}>LLEGADA CLIENTE</th>
+                <th colSpan={2}>ATENCIÓN</th>
+                <th colSpan={2}>DEMORA</th>
+                <th colSpan={5}>AYUDANTE</th>
+                <th colSpan={4}>RELOJERO</th>
+                <th colSpan={1}>TIENDA</th>
+              </tr>
               <tr>
                 <th>Número Simulación</th>
                 <th>Evento</th>
@@ -358,36 +372,71 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              {/* {simulacion.map((simulation, i) => {
-                clientes.map((cliente, index) => {
-                  return (
-                    <tr key={i}>
-                      <td>{simulation.nombre}</td>
-                      <td>{simulation.reloj}</td>
-                      <td>{simulation.llegada.random}</td>
-                      <td>{simulation.llegada.tiempoLlegada}</td>
-                      <td>{simulation.llegada.proximaLlegada}</td>
-                      <td>{simulation.atencion.random}</td>
-                      <td>{simulation.atencion.tipo}</td>
-                      <td>{simulation.demora.random}</td>
-                      <td>{simulation.demora.tiempoDemora}</td>
-                      <td>{simulation.ayudante.estado}</td>
-                      <td>{simulation.ayudante.cola}</td>
-                      <td>{simulation.ayudante.finAtencion}</td>
-                      <td>{simulation.ayudante.tiempoOcupacion}</td>
-                      <td>{simulation.ayudante.tipoOcupacion}</td>
-                      <td>{simulation.relojero.estado}</td>
-                      <td>{simulation.relojero.cola}</td>
-                      <td>{simulation.relojero.finAtencion}</td>
-                      <td>{simulation.relojero.tiempoOcupacion}</td>
-                      <td>{simulation.relojesRetirar}</td>
-                      <td>{cliente.id}</td>
-                      <td>{cliente.state.name}</td>
-                      <td>{cliente.state.description}</td>
-                    </tr>
-                  );
-                });
-              })} */}
+              {simulacion !== null
+                ? simulacion.map((simulation, i) => {
+                    //simulation.clientes.map((cliente, index) => {
+                    return (
+                      <tr key={i}>
+                        <td>{simulation.numeroSimulacion}</td>
+                        <td>{simulation.evento}</td>
+                        <td>{simulation.reloj}</td>
+                        <td>{simulation.llegada.random}</td>
+                        <td>{simulation.llegada.tiempoLlegada}</td>
+                        <td>{simulation.llegada.proximaLlegada}</td>
+                        <td>{simulation.atencion.random}</td>
+                        <td>{simulation.atencion.tipo}</td>
+                        <td>{simulation.demora.random}</td>
+                        <td>{simulation.demora.tiempoDemora}</td>
+                        <td>{simulation.ayudante.estado}</td>
+                        <td>{simulation.ayudante.cola}</td>
+                        <td>{simulation.ayudante.finAtencion}</td>
+                        <td>{simulation.ayudante.tiempoOcupacion}</td>
+                        <td>{simulation.ayudante.tipoOcupacion}</td>
+                        <td>{simulation.relojero.estado}</td>
+                        <td>{simulation.relojero.cola}</td>
+                        <td>{simulation.relojero.finAtencion}</td>
+                        <td>{simulation.relojero.tiempoOcupacion}</td>
+                        <td>{simulation.relojesRetirar}</td>
+                        <td>{simulation.proximoEvento}</td>
+                        <td>
+                          <Button
+                            onClick={() => {
+                              swalClientes
+                                .fire({
+                                  title: <p>Clientes</p>,
+                                  footer: "Copyright 2018",
+                                  didOpen: () => {
+                                    // `MySwal` is a subclass of `Swal`
+                                    //   with all the same instance & static methods
+                                    swalClientes.clickConfirm();
+                                  },
+                                })
+                                .then(() => {
+                                  return swalClientes.fire(
+                                    <>
+                                      {simulation.clientes.map(
+                                        (cliente, index) => {
+                                          return (
+                                            <p style={{ fontSize: "20px" }}>
+                                              Cliente: {cliente.id} Estado:{" "}
+                                              {cliente.state.description}
+                                            </p>
+                                          );
+                                        }
+                                      )}
+                                    </>
+                                  );
+                                });
+                            }}
+                          >
+                            Ver clientes
+                          </Button>
+                        </td>
+                      </tr>
+                    );
+                    //});
+                  })
+                : null}
             </tbody>
           </Table>
         </Row>
